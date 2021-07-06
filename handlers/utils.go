1832c2d0cc83f0f13c0681b5351f71f10537006c
Ethan <ethan.blumenthal@gmail.com>
2021-07-06 14:45:20 -0500

    Created HTTP response helper functions.

---
+++ handlers/utils.go
@@ -0,0 +1,34 @@
+package handlers
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+type Response interface {
+	Json() []byte
+	StatusCode() int
+}
+
+type ErrRes struct {
+	Error string `json:"error"`
+}
+
+func writeErrRes(w http.ResponseWriter, err error) {
+	jsonErrRes, _ := json.Marshal(ErrRes{err.Error()})
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusInternalServerError)
+	w.Write(jsonErrRes)
+}
+
+func writeRes(w http.ResponseWriter, content interface{}) {
+	contentJson, err := json.Marshal(content)
+	if err != nil {
+		writeErrRes(w, err)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(contentJson)
+}
